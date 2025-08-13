8cad466a295a32248d8d866d943be0aeb51c8445
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2025-08-13 14:39:14 -0500

    cookiecutter

---
+++ usb_host_gamepad.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2025 Cooper Dalrymple
+#
+# SPDX-License-Identifier: MIT
+"""
+`usb_host_gamepad`
+================================================================================
+
+CircuitPython USB host driver for game controller devices.
+
+
+* Author(s): Cooper Dalrymple
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/relic-se/CircuitPython_USB_Host_Gamepad.git"
